3437d09afb657d9177a44ce34ad93fa8bb466877
Krzysztof Olma <olmak3910@gmail.com>
2018-01-24 19:15:31 +0100

    Now works

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 var h = prompt('Enter the number of tree higth:');
 
 function drawTree (h) {
-	for (i = 0 ; i <= h ; i++) {
+	for (i = 0 ; i < h ; i++) {
 		var star = '';
 		for (j = 0; j <= i ; j++) {
-			star += '*';
+			var star = star += '*';
 		}
+		//for (k = 0; k <= i; k--) {
+		//	var star = star =+ '';
+		//}
 		console.log(star);
 	}
 };
 
-
-
+drawTree(h);
